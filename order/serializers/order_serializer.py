ba9a112945574493a3f7c08068b14f19a35fccb8
Luan-LopS <luam10@hotmail.com.br>
2025-08-14 11:50:10 -0300

    corrigido problema como PEP517, atulizei o cffi e auterei no nome do diretorio  raiz  do projeto

--- order/serializers/order_serializer.py
+++ order/serializers/order_serializer.py
@@ -5,6 +5,7 @@ from product.serializers.produtc_serializer import ProductSerializer
 
 class OrderSerializer(serializers.ModelSerializer):
     products = ProductSerializer(required=True, many=True)
+    products_id = serializers.PrimaryKeyRelatedField(query=products.objects.all(), write_only=True, many=True)
     total = serializers.SerializerMethodField()
 
     def get_total(self, instance):
@@ -13,4 +14,15 @@ class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ['user', 'products', 'total']
+        fields = ['user', 'products', 'total', 'products_id']
+        extra_kwargs = {'product': {'required': False}}
+
+    def create(self, validated_data):
+        product_data = validated_data.pop('products_id')
+        user_data = validated_data.pop('user')
+
+        order = Order.objects.create(validated_data)
+        for product in product_data:
+            order.products.add(product)
+
+        return order
