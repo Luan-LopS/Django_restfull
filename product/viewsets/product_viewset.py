2bd53d087ae857a4a91a362ee41d9c1cc2ea921f
Luan-LopS <luam10@hotmail.com.br>
2025-09-24 14:38:46 -0300

    ralizado correção de autenticação de product

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -7,8 +7,6 @@ from product.serializers.produtc_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
     def get_queryset(self):
