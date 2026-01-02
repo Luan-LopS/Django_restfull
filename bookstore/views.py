1bb16a278cd3dc793cf4791761560e92812317c9
Luan-LopS <luam10@hotmail.com.br>
2026-01-02 15:30:23 -0300

    fala no CD2

--- bookstore/views.py
+++ bookstore/views.py
@@ -7,23 +7,22 @@ import git
 
 @csrf_exempt
 def update(request):
-    if request.method == "POST":
+    if request.method != "POST":
         return HttpResponse("Method not  allowed", status=405)
     
     if request.headers.get("x-github-Event") != "push":
-        return HttpResponse("Ignored", status=200
+        return HttpResponse("Ignored", status=200)
 
-    try:        
+    try:
         repo = git.Repo("/home/LuanLopS/Django_restfull")
         origin = repo.remotes.origin
 
         origin.pull()
         return HttpResponse("Updated code on PythonAnywhere")
-    except  Exception as e:
+    except Exception as e:
         return HttpResponse(f"Error: {e}", status=500)
 
 
 def hello_world(request):
     template = loader.get_template("hello_world.html")
     return HttpResponse(template.render())
-    
